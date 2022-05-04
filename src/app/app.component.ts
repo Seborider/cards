07516b3fe4e94ce64e92a974f72b1c1939fda473
Seborider <mayer.sebobo@gmail.com>
2022-05-04 20:35:43 +0200

    ⚡️ Add more cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,20 +9,20 @@ export class AppComponent {
   posts = [
     {
       title: 'Neat Tree',
-      imageUrl: 'assets/tree.jpeg',
-      username: '@nature',
+      imageurl: 'assets/tree.jpeg',
+      username: 'nature',
       content: 'Saw this awesome tree during my hike today'
     },
     {
       title: 'Snowy Mountain',
-      imageUrl: 'assets/mountain.jpeg',
-      username: '@mountainlover',
+      imageurl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
       content: 'Here is a picture of a snowy moutain'
     },
     {
       title: 'Mountain Biking',
-      imageUrl: 'assets/biking.jpeg',
-      username: '@biking12222',
+      imageurl: 'assets/biking.jpeg',
+      username: 'biking12222',
       content: 'I did some biking today'
     }
   ]
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardComponent implements OnInit {
   @Input() title = "";
-  @Input() imageUrl = "";
+  @Input() imageurl = "";
   @Input() username = "";
   @Input() content = "";
 
